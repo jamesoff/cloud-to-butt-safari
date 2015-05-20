5f20eb3475cb9677f37b0edfe92cc0f0a6fe01b8
James Seward <jseward@moonfruit.com>
2015-05-20 14:18:03 +0100

    Normalise indentation

--- cloudtobutt.js
+++ cloudtobutt.js
@@ -27,11 +27,11 @@ function walk(node) {
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
 	
-    v = v.replace(/\bThe Cloud\b/g, "My Butt");
+	v = v.replace(/\bThe Cloud\b/g, "My Butt");
 	v = v.replace(/\bThe cloud\b/g, "My butt");
-    v = v.replace(/\bthe Cloud\b/g, "my Butt");
+	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
-		v = v.replace(/Cloud/g, "Butt");
+	v = v.replace(/Cloud/g, "Butt");
 	v = v.replace(/cloud/g, "butt");
 	
 	textNode.nodeValue = v;
